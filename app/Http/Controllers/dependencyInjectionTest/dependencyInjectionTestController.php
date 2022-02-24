a85c6d446963646d5b5f23dae63933696c18ee58
mrinal975 <mrinalmallik1@gmail.com>
2022-02-24 12:03:02 +0600

    first commit

---
+++ app/Http/Controllers/dependencyInjectionTest/dependencyInjectionTestController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\dependencyInjectionTest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class dependencyInjectionTestController extends Controller
+{
+    //
+}
