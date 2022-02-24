871a8090b2b8039f14e405066cdbb7206b6ae822
mrinal975 <mrinalmallik1@gmail.com>
2022-02-24 13:31:29 +0600

    dependency injection controller created

--- app/Http/Controllers/dependencyInjectionTest/dependencyInjectionTestController.php
+++ app/Http/Controllers/dependencyInjectionTest/dependencyInjectionTestController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\dependencyInjectionTest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class dependencyInjectionTestController extends Controller
+class DependencyInjectionTestController extends Controller
 {
     //
 }
